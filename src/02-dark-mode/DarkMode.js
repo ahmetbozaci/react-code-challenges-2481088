73c5423ecd9ce9b8321512e4d3d911173610fc8d
Ahmet Bozaci <ahmt9417@gmail.com>
2022-09-20 00:56:08 +0300

    Update darkmode component

--- src/02-dark-mode/DarkMode.js
+++ src/02-dark-mode/DarkMode.js
@@ -1,8 +1,21 @@
-export default function DarkMode () {
+/** @format */
+
+import { useState } from 'react';
+export default function DarkMode() {
+  const [darkMode, setDarkMode] = useState(false);
+  // const handleClick = (status) => {
+  //   setDarkMode(status)
+  // }
   return (
-    <div className='page'>
-      <button className='dark-mode-button'>Dark Mode</button>
-      <button className='light-mode-button'>Light Mode</button>
+    <div className={`page ${darkMode && 'dark-mode'}`}>
+      {/* //! Too many re-renders. React limits the number of renders to prevent an infinite loop.
+<button className='dark-mode-button' onClick={handleClick(true)}>Dark Mode</button> */}
+      <button className="dark-mode-button" onClick={() => setDarkMode(true)}>
+        Dark Mode
+      </button>
+      <button className="light-mode-button" onClick={() => setDarkMode(false)}>
+        Light Mode
+      </button>
     </div>
-  )
+  );
 }
